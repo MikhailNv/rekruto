a51771cfd0f32735b5779d1db809cdd5b3beb084
UnitFuture <uuranium235@gmail.com>
2022-03-07 01:06:28 +0300

    New main-page design

--- Rekruto/Rekruto/settings.py
+++ Rekruto/Rekruto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-fm5+)wx4#cme!lo_=uapte3*@$6h@-91ntm^k+*j&81cixe+$d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.ngrok.io']
 
 
 # Application definition
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "Rekruto/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
